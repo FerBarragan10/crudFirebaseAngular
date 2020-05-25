aefb45a1b59648393b1ae86c807d061a47164071
FerBarragan10 <fer7_10@hotmail.com>
2020-05-25 01:00:33 -0300

    ruta infoCerveza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent}from './pages/home/home.component';
 import{CervezaComponent} from './pages/cerveza/cerveza.component';
+import{ViewCervezaComponent} from'./pages/view-cerveza/view-cerveza.component';
 
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'cerveza/:id',component:CervezaComponent},
+  {path:'infoCerveza/:id',component:ViewCervezaComponent},
   {path:'**',pathMatch:'full',redirectTo:'home'}
 ];
 
